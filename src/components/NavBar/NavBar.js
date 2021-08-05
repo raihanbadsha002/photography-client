51d406f292cfcebc7c4291c35c89a1480c85ab70
RAIHAN <raihanbadsha363@gmail.com>
2021-08-06 03:30:32 +0600

    2nd-commite

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -24,7 +24,7 @@ const NavBar = () => {
         <nav className="navbar navbar-expand-lg  navbar-dark">
           <div className="container">
             <Link className="navbar-brand" to="/">
-              <span class="text-uppercase">
+              <span className="text-uppercase">
                 <FontAwesomeIcon icon={faCameraRetro} /> Photography
               </span>
             </Link>
